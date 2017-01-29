1040fea819598d8c0825984aed2a93ed0a2862a5
swara <sawant12262@gmail.com>
2017-01-29 22:08:36 +0530

    changes added

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  resources :tasks
-  devise_for :users
+  resources :tasks do
+  	member do
+  		put :change
+  	end
+  end
+   devise_for :users
     
 devise_scope :user do  
    get '/users/sign_out' => 'devise/sessions#destroy'
