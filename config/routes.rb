c8780f5b92757eb20276e04b3fd5be4358687c94
swara <sawant12262@gmail.com>
2017-01-29 18:08:37 +0530

    this is after breakup

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  resources :tasks
+  devise_for :users
+    
+devise_scope :user do  
+   get '/users/sign_out' => 'devise/sessions#destroy'
+  root "pages#home"
+  get 'about' => "pages#about"
+
+end
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
